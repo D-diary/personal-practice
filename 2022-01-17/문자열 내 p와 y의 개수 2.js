57c5bd9115e911c10b129699bf6987ca06a362da
D-diary <dlrlsud@naver.com>
2022-01-17 22:01:45 +0900

    reverse array

--- 2022-01-17/문자열 내 p와 y의 개수 2.js
+++ 2022-01-17/문자열 내 p와 y의 개수 2.js
@@ -9,11 +9,11 @@ function solution(s){
     list = s.toLowerCase();
     console.log('list : ', list)
     arr = Array.from(list)
-    console.log(arr)
+    console.log('arr : ', arr)
     countp = arr.filter(element => `${arr[0]}` === element).length // arr의 첫번째 값
-    console.log(countp)
+    console.log('countp : ', countp)
     county = arr.filter(element => `${arr[arr.length -1]}` === element).length // arr의 마지막 값
-    console.log(county)
+    console.log('county :', county)
     if (countp === county) {
       return true
     }
@@ -23,4 +23,4 @@ function solution(s){
 
 }
 
-console.log(solution(s))
+console.log(solution(s2))
--- 2022-01-17/문자열 내 p와 y의 개수.js
+++ 2022-01-17/문자열 내 p와 y의 개수.js
@@ -12,11 +12,11 @@ function solution(s){
     list = s.toLowerCase(); // 소문자로 변환
     console.log('list : ', list)
     arr = Array.from(list) // 배열로 변환 
-    console.log(arr)
+    console.log('arr :', arr)
     countp = arr.filter(element => 'p' === element).length // p개수 셈
-    console.log(countp)
+    console.log('countp :', countp)
     county = arr.filter(element => 'y' === element).length // y개수 셈
-    console.log(county)
+    console.log('county :', county)
     if (countp === county) {
       return true
     }
