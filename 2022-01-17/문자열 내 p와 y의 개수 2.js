e6fda6e957314e8318f4d581b371546737ca6fe2
D-diary <dlrlsud@naver.com>
2022-01-17 19:28:26 +0900

    2022-01-17

--- 2022-01-17/문자열 내 p와 y의 개수 2.js
+++ 2022-01-17/문자열 내 p와 y의 개수 2.js
@@ -10,9 +10,9 @@ function solution(s){
     console.log('list : ', list)
     arr = Array.from(list)
     console.log(arr)
-    countp = arr.filter(element => `${arr[0]}` === element).length
+    countp = arr.filter(element => `${arr[0]}` === element).length // arr의 첫번째 값
     console.log(countp)
-    county = arr.filter(element => `${arr[arr.length -1]}` === element).length
+    county = arr.filter(element => `${arr[arr.length -1]}` === element).length // arr의 마지막 값
     console.log(county)
     if (countp === county) {
       return true
