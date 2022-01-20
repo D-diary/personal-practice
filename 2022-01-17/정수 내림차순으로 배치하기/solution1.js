c298b22fc6a79ac21d8dfa692ac6e80d042c7559
D-diary <dlrlsud@naver.com>
2022-01-20 23:02:20 +0900

    1

--- 2022-01-17/정수 내림차순으로 배치하기/solution1.js
+++ 2022-01-17/정수 내림차순으로 배치하기/solution1.js
@@ -5,12 +5,12 @@ function solution(n) {
   const arr = (n+"").split('');
   arr.sort(function(a,b){return b-a});    
   for ( let i = 0; i < arr.length; i++) {
-    arr[i] = Number(arr[i])
+    arr[i] = Number(arr[i]);
   }
-  const arr1 = arr.join('')
-  const result = Number(arr1) 
-  return result
+  const arr1 = arr.join('');
+  const result = Number(arr1);
+  return result;
 }
 
 
-console.log(solution(118372)) // 873211
+console.log(solution(118372)); // 873211
