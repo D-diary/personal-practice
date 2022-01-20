405facb6b6ffd135c781c711b7febf226dc4f773
D-diary <dlrlsud@naver.com>
2022-01-20 17:55:32 +0900

    정수 내림차순 다시풀기

--- 2022-01-17/정수 내림차순으로 배치하기/solution1.js
+++ 2022-01-17/정수 내림차순으로 배치하기/solution1.js
@@ -1,10 +1,27 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12933
 
+
 function solution(n) {
-    const arr = (n+"").split('');
-    arr.sort(function(a,b){return b-a});    
-    var answer = Number(arr.toString().replace(/\,/g,''));
-    return answer;
+  const arr = (n+"").split('');
+  console.log(arr)
+  arr.sort(function(a,b){return b-a});    
+  console.log(arr)
+  for ( let i = 0; i < arr.length; i++) {
+    arr[i] = Number(arr[i])
+  }
+  return arr
 }
 
+
 console.log(solution(118372))
+
+// 여쭤보고 싶은 코드
+
+// function solution(n) {
+//     const arr = (n+"").split('');
+//     arr.sort(function(a,b){return b-a});    
+//     var answer = Number(arr.toString().replace(/\,/g,''));
+//     return answer;
+// }
+
+// console.log(solution(118372))
