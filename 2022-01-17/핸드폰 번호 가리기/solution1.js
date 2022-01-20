2bdc5ce0b76d4cfb684d87b43454781d3c42f0e7
D-diary <dlrlsud@naver.com>
2022-01-20 23:50:23 +0900

    3

--- 2022-01-17/핸드폰 번호 가리기/solution1.js
+++ 2022-01-17/핸드폰 번호 가리기/solution1.js
@@ -1,10 +1,9 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12948
 
-
+// 1차 실패 
 function solution(phone_number) {
   let p = phone_number
   let answer = '';
-  console.log(p[p.length-1])
   for (let i = 0; i < p.length; i++) {
     if (p[i] != p[p.length-1]) {
       iter = p[i].replace('${p[i]}','*')
