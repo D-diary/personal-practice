ac3bb1146c64da47ca1561ac3ce5cbb295a20802
D-diary <dlrlsud@naver.com>
2022-01-22 17:10:54 +0900

    save2

--- 2022-01-22/제일 작은 수 제거하기/solution1.js
+++ 2022-01-22/제일 작은 수 제거하기/solution1.js
@@ -2,32 +2,39 @@
 
 // 코드 실행 통과 채점 X
 
-function solution(arg){
+function solution(arg) {
   let temp = 10;
   let arr = [];
   let newArr = [];
-  for (let i = 0; i < arg.length; i++) {
+  if (arg.length > 2) {
+    for (let i = 0; i < arg.length; i++) {
     arr.push(arg[i])
     if (temp > arg[i]) {
       temp = arg[i]
     }
+    }
+    arr = String(arr)
+    // temp = String(temp)
+    arr = arr.replace(temp,'');
+    for (let i = 0; i < arr.length; i++) {
+      arr = arr.replace(',','')
+      
+    }
+    // arr.pop()
+    for (let i = 0; i < arr.length; i++) {
+      newArr[i] = Number(arr[i])
+    }
+    console.log(newArr)
+    return arr
   }
-  arr = String(arr)
-  // temp = String(temp)
-  arr = arr.replace(temp,'');
-  for (let i = 0; i < arr.length; i++) {
-    arr = arr.replace(',','')
-    
+  
+  
+  if (arg.length <= 1) {
+    return [-1]
   }
-  // arr.pop()
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = Number(arr[i])
-  }
-  console.log(arr)
-  return arr
 }
 
-console.log(solution([4,3,2,1])); // [4, 3, 2]
-console.log(solution([10])); // [-1]
+console.log(solution([4,3,2,1]))
+console.log(solution([10]))
 
 
