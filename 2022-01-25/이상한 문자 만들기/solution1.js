fced49214bfaaae84911a90b5e6dacea87e6fe39
D-diary <dlrlsud@naver.com>
2022-01-25 22:48:38 +0900

    2022-01-25

--- 2022-01-25/이상한 문자 만들기/solution1.js
+++ 2022-01-25/이상한 문자 만들기/solution1.js
@@ -1,13 +1,23 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12930
 
 function solution(s) {
-
-  for(let i = 0; s.length; i++) {
-    if ( i % 2 ===0 ) {
-      s.toUpperCase()
-    } else {
-      s.toLowerCase()
+  let idx = 0;
+  let result = new String();
+  for (let i = 0; i < s.length; i++) {
+    
+    if(s[i]===" ") {
+      idx = 0;
     }
-  }
-  }
+    
+    if (idx%2===0) {
+      s[i].toUpperCase()
+      console.log('up',s[i],idx,i)
+    } else {
+      s[i].toLowerCase()
+      console.log('lo',s[i],idx,i)
+    }
+    idx ++;
+  }  
 }
+
+console.log(solution("try hello world"))
--- 2022-01-25/자릿수 더하기/solution1.js
+++ 2022-01-25/자릿수 더하기/solution1.js
@@ -1,5 +1,8 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12931
+
 function solution(n) {
-  n = String(n)
+  n = String(n);
+  
   let num = Number(n[0]);
 
   for (let i = 0; i < n.length - 1; i++) {
