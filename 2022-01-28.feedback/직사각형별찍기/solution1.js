9ddcf5f2133b1a95c388271e386df3b9d744441a
D-diary <dlrlsud@naver.com>
2022-02-04 23:47:54 +0900

    2022-02-04

--- 2022-01-28.feedback/직사각형별찍기/solution1.js
+++ 2022-01-28.feedback/직사각형별찍기/solution1.js
@@ -1,7 +1,20 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12969?language=javascript
 
 function solution(x, y) {
+  let star = [];
+  
+  for (let i = 0; i < y; i++) {
+    console.log('i',i);
 
+    for (let j = 0; j < x; j++) {
+      console.log('*');
+      star.push('*');
+      console.log('j',j);
+    }
+  }
+
+  result = star.join('')
+  console.log(result)
 }
 
 console.log(solution(5, 3));
@@ -9,7 +22,7 @@ console.log(solution(5, 3));
 // *****
 // *****
 
-console.log(solution(3, 5));
+// console.log(solution(3, 5));
 // ***
 // ***
 // ***
