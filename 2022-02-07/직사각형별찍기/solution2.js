74b3a8de2ea6a4185a2ea85c0d244bcb107064fe
D-diary <dlrlsud@naver.com>
2022-02-07 14:10:33 +0900

    2022-02-07

---
+++ 2022-02-07/직사각형별찍기/solution2.js
@@ -0,0 +1,32 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12969?language=javascript
+
+// vscode에서는 정상적으로 출력되나 프로그래머스에서 output size differs
+
+function solution(x, y) {
+  let star = '';
+  
+  for (let i = 0; i < y; i++) {
+    for (let j = 0; j < x; j++) {
+      // console.log('*');
+      star += '*'
+    }
+    star += `\n`
+  }
+
+  // result = star.join('')
+  // console.log('re',result);
+  // console.log('star',star);
+  return star;
+}
+
+console.log(solution(5, 3));
+// *****
+// *****
+// *****
+
+console.log(solution(3, 5));
+// ***
+// ***
+// ***
+// ***
+// ***
