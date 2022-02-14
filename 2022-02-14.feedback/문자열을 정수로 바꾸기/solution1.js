dd378323b135d99fb9aa2ac72cfa2909417a1034
D-diary <dlrlsud@naver.com>
2022-02-14 19:57:48 +0900

    2022-02-14

---
+++ 2022-02-14.feedback/문자열을 정수로 바꾸기/solution1.js
@@ -0,0 +1,12 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12925
+
+function solution(s) {
+  let answer = 0;
+
+  answer = Number(s);
+
+  return answer;
+}
+
+console.log(solution("1234")); // 1234
+console.log(solution("-1234")); // -1234
