761960234db119fff2bef9ae1484f5fda972d50c
D-diary <dlrlsud@naver.com>
2022-02-15 15:03:52 +0900

    2022-02-15

--- 2022-02-14.feedback/문자열을 정수로 바꾸기/substring.js
+++ 2022-02-14.feedback/문자열을 정수로 바꾸기/substring.js
@@ -1,5 +1,5 @@
 const str = 'abcdef';
 
-console.log(str.substring(1));
-console.log(str.substring(3));
-console.log(str.substring(2, 4));
+console.log(str.substring(1)); // 1번 인덱스부터 끝까지 
+console.log(str.substring(3)); // 3번 인덱스부터 끝까지
+console.log(str.substring(2, 4)); // 2번 인덱스부터 4번 앞(3까지)
