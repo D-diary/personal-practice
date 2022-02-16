729f553301c3835aaefe1130f0555ddedf850d9c
D-diary <dlrlsud@naver.com>
2022-02-16 17:39:27 +0900

    2022-02-16

--- 2022-02-16/시저 암호/solution1.js
+++ 2022-02-16/시저 암호/solution1.js
@@ -2,17 +2,33 @@
 
 function solution(s, n) {
   let answer = '';
-  const alpha = 'abcdefghijklmnopqrstuvwxyz';
+  const lower = 'abcdefghijklmnopqrstuvwxyz';
+  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-  for ( let i = 0; i < alpha.length; i++) {
-    if ( n === alpha[i]) {
-      console.log('yes')
+  for ( let i = 0; i < lower.length; i++) {
+    for ( let j = 0; j < s.length; j++) {
+      if ( s[j] === lower[i]) {
+        answer += (lower[i+n]);
+      }
     }
   }
+  
+  for ( let i = 0; i < upper.length; i++) {
+    for ( let j = 0; j < s.length; j++) {
+      if ( s[j] === upper[i]) {
+        answer += (upper[i+n]);
+      }
+    }
+  }
+  //  25 + 1 = 26 > 25
 
   return answer;
 }
 
-console.log(solution("AB", 1)); // "BC"
-console.log(solution("z", 1)); // "a"
-console.log(solution("a B z", 4)); // "e F d"
+// console.log(solution("ab", 1)); // "bc"
+
+console.log(solution("Z", 1)); // "A"
+
+// console.log(solution("AB", 1)); // "BC"
+// console.log(solution("z", 1)); // "a"
+// console.log(solution("a B z", 4)); // "e F d"
