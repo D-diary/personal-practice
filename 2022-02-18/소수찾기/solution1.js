f2f22bac9bec49904971ccee8daa1b2bd5c90f75
D-diary <dlrlsud@naver.com>
2022-02-17 20:08:37 +0900

    2022-02-17

--- 2022-02-18/소수찾기/solution1.js
+++ 2022-02-18/소수찾기/solution1.js
@@ -1,16 +1,25 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12921
 
 function solution(n) {
-  let answer = 0;
-
-  for ( let i = 0; i <= n; i++) {
-    if ( i % i === 0 ) {
-      console.log(i)
+	const arr = [];  
+  
+  for(let i = 2; i <= n; i++) {
+    let count = 0;
+    for (let j = 1; j <= i; j++) {
+      if( i % j === 0 ) {
+      	count = count + 1;
+      	console.log('i',i, 'j', j, 'count', count)
+    	}
     }
-  }
+    if( count === 2 ) {
+      arr.push(i);
+    }
 
-  return answer;
+    // console.log('i',i , 'count', count)
+  }
+  console.log('arr',arr)
+  return arr.length;
 }
 
-console.log(solution(10)); // 4
+console.log(solution(10)) // 4
 console.log(solution(5)); // 3
