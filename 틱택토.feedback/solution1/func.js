4c85444d18025beaf441a054abaf0523094f8ffd
D-diary <dlrlsud@naver.com>
2022-02-28 20:31:56 +0900

    2022-02-28

---
+++ 틱택토.feedback/solution1/func.js
@@ -0,0 +1,16 @@
+function f1() {
+    const x = 10;
+    let y = 20;
+    let z = 30;
+
+    let result = 0;
+    result += x + y + z;
+    console.log(result);
+}
+
+f1();
+f1();
+f1();
+f1();
+f1();
+f1();
