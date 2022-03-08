1b387272fb3f535a225d6868d5619c18b0a3b054
D-diary <dlrlsud@naver.com>
2022-03-08 20:48:35 +0900

    2022-03-08

---
+++ 2022-03-09/셔플함수를만드시오/solution1.js
@@ -0,0 +1,18 @@
+// 셔플 함수를 만드시오.
+
+function random(end) {
+  return Math.floor(Math.random() * end);
+}
+
+function solution(arr) {
+  const r = random(arr.length)
+  let result = [];
+  for ( let i = 0; i < arr.length; i++ ) {
+    result.push(arr.splice(r, 1)[0])
+    // result.push(arr[random(arr.length)]);
+    console.log(i)
+  }
+  return result;
+}
+
+console.log(solution([1, 2, 3, 4, 5])); // [3, 2, 1, 4, 5] 결과는 실행마다 달라질 수 있음.
